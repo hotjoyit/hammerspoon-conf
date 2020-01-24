a8d6796fe2485b936139ec64a3b70a5f142e38b8
hotjoyit <hotjoyit@gmail.com>
2020-01-24 20:37:48 +0900

    F17 단축키를 사용한 앱 실행기를 만들어라

--- init.lua
+++ init.lua
@@ -1,7 +1,9 @@
 require('modules.inputsource_aurora')
 
 hs.hotkey.bind({'option', 'cmd'}, 'r', hs.reload)
+local f17_mode = hs.hotkey.modal.new()
 
+hs.hotkey.bind({}, 'f17', function() f17_mode:enter() end, function() f17_mode:exit() end)
 
 function vimesc()
   local inputEnglish = "com.apple.keylayout.ABC"
@@ -17,3 +19,26 @@ end
 hs.hotkey.bind({}, 'escape', vimesc)
 
 hs.alert.show('Hammerspoon Reloaded')
+
+--- A closure function
+function open(name)
+  return function()
+    hs.application.launchOrFocus(name)
+    if name == 'Finder' then
+      hs.appfinder.appForName(name):activate()
+    end
+  end
+end
+
+do -- app toggle
+    local mode = f17_mode;
+
+    mode:bind({}, 'J', open("IntelliJ Idea"))
+    mode:bind({}, 'N', open("Notion"))
+    mode:bind({}, 'C', open("Google Chrome"))
+    mode:bind({}, 'T', open("iTerm"))
+    mode:bind({}, 'R', open("Reminders"))
+    mode:bind({}, 'I', open("Music"))
+    mode:bind({}, 'K', open("KakaoTalk"))
+    mode:bind({}, 'F', open("Firefox"))
+end
